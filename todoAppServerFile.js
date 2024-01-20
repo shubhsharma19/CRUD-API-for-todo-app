a278124fef7c055bf4e0ce8cc841f44e3ddb7870
Suhas Sumukh <70210383+suhasasumukh@users.noreply.github.com>
2024-01-20 23:37:21 +0530

    Update todoAppServerFile.js

--- todoAppServerFile.js
+++ todoAppServerFile.js
@@ -1,10 +1,6 @@
-// this writes data into a file so that data is saved even after server is closed
-
-// basically an offline version of todoServer
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
@@ -14,113 +10,105 @@ app.use(bodyParser.json());
 
 const filePath = path.join(__dirname, '/minidatabase.json');
 
-// return list of all todos 
-app.get('/todos', (req, res) => {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            res.status(500);
-        }
+app.get('/todos', async (req, res) => {
+    try {
+        const data = await fs.readFile(filePath, 'utf-8');
         res.json(JSON.parse(data));
-    })
-});
-
-// add a new todo 
-app.post('/todos', (req, res) => {
-    const randomID = Math.floor(Math.random() * 100000) + 1;
-    const newTodo = {
-        "title": req.body.title,
-        "id": randomID,
-        "description": req.body.description,
-        "isCompleted": req.body.isCompleted
+    } catch (error) {
+        res.status(500).send({ error: 'Internal Server Error' });
     }
+});
 
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) throw err;
+app.post('/todos', async (req, res) => {
+    try {
+        const data = await fs.readFile(filePath, 'utf-8');
         const todosList = JSON.parse(data);
+        const randomID = Math.floor(Math.random() * 100000) + 1;
+        const newTodo = {
+            title: req.body.title,
+            id: randomID,
+            description: req.body.description,
+            isCompleted: req.body.isCompleted || false
+        };
+
         todosList.push(newTodo);
-        fs.writeFile(filePath, JSON.stringify(todosList), (err) => {
-            if (err) throw err;
-            res.json({ msg: "Added a todo successfully!" })
-        })
-    })
+        await fs.writeFile(filePath, JSON.stringify(todosList));
+        res.status(201).json(newTodo);
+    } catch (error) {
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
 });
 
-// Retrieve a specific todo item by ID 
-app.get('/todos/:id', (req, res) => {
-    fs.readFile(filePath, "utf-8", (err, data) => {
-        if (err) {
-            res.status(500);
+app.get('/todos/:id', async (req, res) => {
+    try {
+        const data = await fs.readFile(filePath, 'utf-8');
+        const todosList = JSON.parse(data);
+        const targetTodo = findTodoByID(todosList, parseInt(req.params.id));
+
+        if (!targetTodo) {
+            res.sendStatus(404);
         } else {
-            let todosList = JSON.parse(data);
-            const targetTodo = findTodoByID(todosList, parseInt(req.params.id));
-            if (!targetTodo) {
-                res.sendStatus(404);
-            } else {
-                res.json(targetTodo);
-            }
+            res.json(targetTodo);
         }
-    })
+    } catch (error) {
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
 });
 
-
-// update a specific todo by id 
-app.put('/todos/:id', (req, res) => {
-    const targetID = parseInt(req.params.id);
-    fs.readFile(filePath, "utf-8", (err, data) => {
+app.put('/todos/:id', async (req, res) => {
+    try {
+        const data = await fs.readFile(filePath, 'utf-8');
         let todosList = JSON.parse(data);
+        const targetID = parseInt(req.params.id);
         const targetTodoIndex = todosList.findIndex(item => item.id === targetID);
+
         if (targetTodoIndex === -1) {
-            res.status(404).json({ error: "todo not found!" });
+            res.status(404).json({ error: 'Todo not found!' });
         } else {
-            todosList[targetTodoIndex].title = req.body.title;
-            todosList[targetTodoIndex].description = req.body.description;
-            todosList[targetTodoIndex].isCompleted = req.body.isCompleted;
-            fs.writeFile(filePath, JSON.stringify(todosList), (err) => {
-                if (err) throw err;
-                res.json(todosList);
-            })
+            todosList[targetTodoIndex] = {
+                ...todosList[targetTodoIndex],
+                title: req.body.title,
+                description: req.body.description,
+                isCompleted: req.body.isCompleted
+            };
+
+            await fs.writeFile(filePath, JSON.stringify(todosList));
+            res.json({ msg: 'Todo updated successfully!' });
         }
-    });
+    } catch (error) {
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
 });
 
-// delete a specific todo
-app.delete('/todos/:id', (req, res) => {
-    const targetID = parseInt(req.params.id);
-
-    // reading the file for todolist array
-    fs.readFile(filePath, "utf-8", (err, data) => {
+app.delete('/todos/:id', async (req, res) => {
+    try {
+        const data = await fs.readFile(filePath, 'utf-8');
         let todosList = JSON.parse(data);
+        const targetID = parseInt(req.params.id);
         const targetTodoIndex = todosList.findIndex(item => item.id === targetID);
+
         if (targetTodoIndex === -1) {
-            res.status(404).json({ error: "todo not found" });
+            res.status(404).json({ error: 'Todo not found!' });
         } else {
             if (todosList.length) {
-                // splicing element from array
                 todosList.splice(targetTodoIndex, 1);
-                // writing to the file with updated array
-                fs.writeFile(filePath, JSON.stringify(todosList), (err) => {
-                    if (err) {
-                        res.status(500);
-                    }
-                    res.status(200).json({ "msg": "Deleted the todo!" });
-                })
+                await fs.writeFile(filePath, JSON.stringify(todosList));
+                res.status(200).json({ msg: 'Deleted the todo!' });
             } else {
-                res.status(400).json({ error: "todo list is empty!" });
+                res.status(400).json({ error: 'Todo list is empty!' });
             }
         }
-    });
+    } catch (error) {
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
 });
 
-// for all other routes
 app.all('*', (req, res) => {
     res.sendStatus(404);
-})
+});
 
 function findTodoByID(todosList, targetID) {
-    const targetTodoObject = todosList.find(
-        item => item.id === targetID
-    );
-    return targetTodoObject;
+    return todosList.find(item => item.id === targetID);
 }
 
 app.listen(PORT);
